c1313a58c33ee1b7033a55f88650ecffe70ade7a
victor0198ebs <victor.ebs0198@gmail.com>
2019-05-15 17:56:31 +0300

    models created

---
+++ apps/task/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Task(models.Model):
+    user_created = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_assigned = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.IntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ManyToManyField(Task, on_delete=models.CASCADE)
+    comment = models.ManyToManyField(Comment, on_delete=models.CASCADE)
