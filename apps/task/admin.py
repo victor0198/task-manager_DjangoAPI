e987fda3dc378410ac6cdb2b2848e215bd1a1bb4
Eugene Stucalov <eugene.stuc@gmail.com>
2019-05-16 10:05:51 +0300

    add url in admin.py

--- apps/task/admin.py
+++ apps/task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.task.models import Task, Comment, Notification
+
+admin.site.register(Task)
+admin.site.register(Comment)
+admin.site.register(Notification)
